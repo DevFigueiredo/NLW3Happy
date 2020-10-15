cb73196dfb0c1ef40e639cd765d4d426e1499ee5
suporte <suporte@JAA.local>
2020-10-14 22:52:23 -0300

    Criado as rotas e controllers sobre cada orfanato

--- backend/src/database/migrations/1602723282811-create_orphanages.ts
+++ backend/src/database/migrations/1602723282811-create_orphanages.ts
@@ -6,12 +6,13 @@ export class createOrphanages1602723282811 implements MigrationInterface {
      await queryRunner.createTable(new Table({
      name: 'orphanages',
      columns:[    
-{name: 'id', type: 'integer', unsigned: true, isPrimary: true, isGenerated:true, generationStrategy:'increment'},
+{name:'id', type: 'integer', unsigned: true, isPrimary: true, isGenerated:true, generationStrategy:'increment'},
 {name:'name', type: 'varchar'},
 {name:'latitude', type:'decimal', scale: 10, precision: 2},
 {name:'longitude', type:'decimal', scale: 10, precision: 2},
 {name:'about', type:'text'},
 {name:'instructions', type:'text'},
+{name:'opening_hours', type: 'varchar'},
 {name:'open_on_weekends', type:'boolean', default: false}
 
 
