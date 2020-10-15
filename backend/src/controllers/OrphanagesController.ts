2bd7703a7ae1f635c45b04f406d83a3ba3eedf21
suporte <suporte@JAA.local>
2020-10-15 15:33:04 -0300

    Adicionado as views de validação de dados

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,18 +1,21 @@
 import {Request, Response} from 'express';
 import {getRepository} from 'typeorm';
 import Orphanage from '../models/Orphanage';
-export default {
-    async show(req: Request, res: Response){
+import OrphanageView from '../views/orphanages_view';
 
+export default {
+   async show(req: Request, res: Response){
+     
         const { id } = req.params;
         const orphanagesRepository = getRepository(Orphanage);
-        const orphanages = await orphanagesRepository.findOneOrFail(id);
-        return res.json(orphanages);
-        },
+        const orphanages = await orphanagesRepository.findOneOrFail(id, { relations:['images']});
+        return res.json(OrphanageView.render(orphanages));
+   },
+
    async index(req: Request, res: Response){
    const orphanagesRepository = getRepository(Orphanage);
-   const orphanages = await orphanagesRepository.find();
-   return res.json(orphanages);
+   const orphanages = await orphanagesRepository.find({ relations:['images']});
+   return res.json(OrphanageView.renderMany(orphanages));
    },
 
 
@@ -26,7 +29,13 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends} = req.body;
-            
+
+
+        const requestImages = req.files as Express.Multer.File[];
+        const images = requestImages.map(image=>{
+            return {path: image.filename}
+        })
+
             const orphanagesRepository = getRepository(Orphanage);
             
             const orphanage = orphanagesRepository.create({
@@ -37,7 +46,8 @@ export default {
                 about,
                 instructions,
                 opening_hours,
-                open_on_weekends
+                open_on_weekends,
+                images
             })
         
             await orphanagesRepository.save(orphanage);
